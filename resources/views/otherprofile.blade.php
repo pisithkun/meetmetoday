2e66dd2abe888f4c11af383eadd73373c424bb73
pisithkun <141993071+pisithkun@users.noreply.github.com>
2023-09-20 21:00:13 +0900

    Update otherprofile.blade.php

--- resources/views/otherprofile.blade.php
+++ resources/views/otherprofile.blade.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<!---Coding By CoderGirl | www.codinglabweb.com--->
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>MeetMeToday | My Profile</title>
-    <!---Custom CSS File--->
-    <link rel="stylesheet" href="/app.css" />
-    <script
-      src="https://kit.fontawesome.com/776e4f6f1b.js"
-      crossorigin="anonymous"
-    ></script>
-  </head>
+<x-layout>
 
   <body style="background-color: white">
     <div class="my-pro-container">
@@ -87,12 +73,4 @@
           <p name="Aboutme">{{$user->Aboutme}}</p>
         </div>
       </div>
-    <!-- footer begins -->
-    <footer class="pro-footer">
-      <p>
-        Copyright &copy; 2022 <a href="/" class="text-muted">MeetMeToday</a>.
-        All rights reserved.
-      </p>
-    </footer>
-  </body>
-</html>
+</x-layout>
