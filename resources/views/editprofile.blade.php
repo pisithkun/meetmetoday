1026be6da906450650ac4baca0b54e73863555ed
pisithkun <141993071+pisithkun@users.noreply.github.com>
2023-09-20 20:58:52 +0900

    Update editprofile.blade.php

--- resources/views/editprofile.blade.php
+++ resources/views/editprofile.blade.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<!---Coding By CoderGirl | www.codinglabweb.com--->
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>MeetMeToday | My Profile</title>
-    <!---Custom CSS File--->
-    <link rel="stylesheet" href="/app.css" />
-    <script
-      src="https://kit.fontawesome.com/776e4f6f1b.js"
-      crossorigin="anonymous"
-    ></script>
-  </head>
+<x-layout>
 
   <body style="background-color: white">
     <div class="my-pro-container">
@@ -171,14 +157,4 @@
     <h1 class="No-People-around">No available users found</h1>
     @endif
 
-
-
-    <!-- footer begins -->
-    <footer class="pro-footer">
-      <p class="">
-        Copyright &copy; 2022 <a href="/" class="text-muted">MeetMeToday</a>.
-        All rights reserved.
-      </p>
-    </footer>
-  </body>
-</html>
+</x-layout>
